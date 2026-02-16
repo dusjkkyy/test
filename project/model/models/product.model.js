695cbab4d26a14581c2541465e229f3e99792a86
Lakshay Purwal <purwallakshay2@gmail.com>
2026-02-16 16:31:39 +0530

    Product

--- project/model/models/product.model.js
+++ project/model/models/product.model.js
@@ -1,19 +1,43 @@
-const mongoose = require('mongoose');
-const productSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    price:{
-        type:Number,
-        required:true
-    },
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
-    },
+const mongoose = require ('mongoose');
+const varientSchema = new mongoose.Schema({
+   brand:{
+    type:String,
+    required:true
+   },
+   specifications:{
+    type:Map,
+    required:true
+   },
+   price:{
+    type:Number,
+    required:true
+   },
+   category:{
+    type:String,
+    required:true
+   },
+   stock:{
+    type:Number,
+    default:0
+   }
 });
-const productModel = mongoose.model('product',productSchema);
+
+
+
+
+const productSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    required:true
+  },
+  description:String,
+
+  variants : [varientSchema]
+
+});
+
+const productModel = mongoose.model("Product",productSchema);
+
 module.exports = {
     productModel
 }
